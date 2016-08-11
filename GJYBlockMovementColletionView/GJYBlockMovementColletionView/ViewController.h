54e017449bf2badb95e32e3b8a0fc732077bcad1
DongBeiG <18612784893@163.com>
2016-08-11 13:58:30 +0800

    项目创建

---
+++ GJYBlockMovementColletionView/GJYBlockMovementColletionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GJYBlockMovementColletionView
+//
+//  Created by GJYipode on 16/8/11.
+//  Copyright © 2016年 GJYipode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GJYBlockMovementColletionView/GJYBlockMovementColletionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GJYBlockMovementColletionView
+//
+//  Created by GJYipode on 16/8/11.
+//  Copyright © 2016年 GJYipode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
