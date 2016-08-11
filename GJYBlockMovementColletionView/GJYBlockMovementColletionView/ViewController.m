410a4e25ba560747ae73fec05bf1868cd34093c1
DongBeiG <18612784893@163.com>
2016-08-11 14:20:37 +0800

    简单实现布局

--- GJYBlockMovementColletionView/GJYBlockMovementColletionView/ViewController.m
+++ GJYBlockMovementColletionView/GJYBlockMovementColletionView/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+static NSString *const kCell = @"kCell";
+
+@interface ViewController () <UICollectionViewDelegate , UICollectionViewDataSource>
+
+@property (nonatomic , strong) UICollectionView * colletionView;
+
+@property (nonatomic , strong) UICollectionViewFlowLayout * layout;
 
 @end
 
@@ -16,12 +23,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor blueColor];
+    
+    [self.view addSubview:self.colletionView];
+    
+    _colletionView.delegate = self;
+    _colletionView.dataSource = self;
+    
+    [_colletionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:kCell];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UIColletionViewDatasouce 
+- (NSInteger) collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (NSInteger) numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return  2;
+    
+}
+
+- (UICollectionViewCell *) collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:kCell forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor colorWithRed:((float)arc4random_uniform(256) / 255.0) green:((float)arc4random_uniform(256) / 255.0) blue:((float)arc4random_uniform(256) / 255.0) alpha:1.0];
+    
+    return cell;
+}
+
+
+- (UICollectionView *)colletionView {
+    if (_colletionView == nil) {
+        
+        _layout = [UICollectionViewFlowLayout new];
+        
+        _layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+        
+        _layout.sectionInset = UIEdgeInsetsMake(40, 10, 10, 10);
+        
+        _layout.itemSize = CGSizeMake(80, 80);
+        
+        _colletionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:_layout];
+    }
+    return _colletionView;
 }
 
 @end
